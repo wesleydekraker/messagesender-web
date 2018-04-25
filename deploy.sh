71008d509cab6c1505f79ac2fe669b326b673937
Wesley de Kraker <github@wesleydekraker.com>
2018-04-25 21:58:56 +0200

    Changed build order of projects

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,9 @@ docker rm friendspammerweb_mongo_1 &
 docker rm friendspammerweb_app_1
 docker rmi friendspammerweb_app &
 
-mvn clean install
-
 cd ~/Projects/friendspammer-helper
 mvn clean install
 
 cd ~/Projects/friendspammer-web
+mvn clean install
 docker-compose up -d
